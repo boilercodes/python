1ffb5e84604f1ce2e946672b2ba408657d0101ed
Rami Menai <rami.menai@outlook.com>
2021-08-15 16:59:24 +0300

    Removed TRACE level

--- python_structure/config.py
+++ python_structure/config.py
@@ -1,15 +1,5 @@
-from datetime import datetime
 from os import environ
-from pathlib import Path
 
 
 class Settings:
     debug = environ.get("DEBUG", "").lower() == "true"
-
-
-class Logs:
-    path = Path("python_structure/logs")
-    name = f"{datetime.today().strftime('%d-%m-%Y')}.log"
-
-    fmt = "%(asctime)s - %(name)s %(levelname)s: %(message)s"
-    datefmt = "%D %H:%M:%S"
