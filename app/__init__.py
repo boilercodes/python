bc9b54a617a195763ca9a15755994ef8475b76f8
rmenai <rami.menai@outlook.com>
2021-12-05 19:41:18 +0300

    Update logging initialisation

--- app/__init__.py
+++ app/__init__.py
@@ -1,58 +1,51 @@
-try:
-    from dotenv import load_dotenv
-
-    load_dotenv()
-except ModuleNotFoundError:
-    pass
-
 import logging.handlers
 import os
 from datetime import datetime
 from pathlib import Path
 
-from app.config import Settings
+from app.core import settings
 
-# Set up file logging
-log_dir = Path("app/logs")
-log_file = log_dir / f"{datetime.today().strftime('%d-%m-%Y')}.log"
+# Set up file logging.
+log_dir = Path(f"{__name__}/logs")
+log_file = log_dir / f"{datetime.now().strftime('%d-%m-%Y')}.log"
 os.makedirs(log_dir, exist_ok=True)
 
-# File handler rotates logs every 5 MB
+# File handler rotates logs every 5 MB.
 file_handler = logging.handlers.RotatingFileHandler(
     log_file, maxBytes=5 * (2 ** 20), backupCount=10, encoding="utf-8",
 )
 file_handler.setLevel(logging.DEBUG)
 
-# Console handler prints to terminal
+# Console handler prints to terminal.
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG if Settings.debug else logging.INFO)
+console_handler.setLevel(logging.DEBUG if settings.debug else logging.INFO)
+
+# Format configuration.
+fmt = "%(asctime)s - %(name)s %(levelname)s: %(message)s"
+datefmt = "%H:%M:%S"
 
 # Add colors for logging if available.
 try:
     from colorlog import ColoredFormatter
 
     console_handler.setFormatter(
-        ColoredFormatter(fmt="%(log_color)s%(asctime)s - %(name)s %(levelname)s: %(message)s", datefmt="%D %H:%M:%S"))
+        ColoredFormatter(fmt=f"%(log_color)s{fmt}", datefmt=datefmt))
 except ModuleNotFoundError:
     pass
 
-# Remove old loggers, if any
+# Remove old loggers, if any.
 root = logging.getLogger()
 if root.handlers:
     for handler in root.handlers:
         root.removeHandler(handler)
 
-# Silence irrelevant loggers
-logging.getLogger("").setLevel(logging.ERROR)
+# Silence irrelevant loggers.
+# logging.getLogger("asyncio").setLevel(logging.ERROR)
 
-# Setup new logging configuration
+# Setup new logging configuration.
 logging.basicConfig(
-    format="%(asctime)s - %(name)s %(levelname)s: %(message)s",
-    datefmt="%D %H:%M:%S",
+    format=fmt,
+    datefmt=datefmt,
     level=logging.DEBUG,
     handlers=[console_handler, file_handler]
 )
-
-log = logging.getLogger()
-
-log.debug("Logging initialization complete")
